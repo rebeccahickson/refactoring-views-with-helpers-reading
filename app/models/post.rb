c0d63f5081dc51f12218e4348abe41113aa679ad
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-08 15:22:13 -0500

    commit tree 969610d10040bd697578741ae351e44cb94b798e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base
-
   validate :is_title_case
   before_validation :make_title_case
   belongs_to :author
@@ -7,12 +6,10 @@ class Post < ActiveRecord::Base
   private
 
   def is_title_case
-    if title.split.any?{|w|w[0].upcase != w[0]}
-      errors.add(:title, "Title must be in title case")
-    end
+    errors.add(:title, 'Title must be in title case') if title.split.any? { |w| w[0].upcase != w[0] }
   end
 
   def make_title_case
-    self.title = self.title.titlecase
+    self.title = title.titlecase
   end
 end
